59352a914ef5491566dbfac2bb297a66640f5901
Dasha <dellachaise@gmail.com>
2017-02-28 16:26:36 +0200

    added permisssions

--- resume/apps/pages/api/views.py
+++ resume/apps/pages/api/views.py
@@ -2,6 +2,7 @@ import time
 
 from rest_framework import viewsets
 from rest_framework.exceptions import APIException
+from rest_framework.permissions import AllowAny
 
 from resume.apps.pages.models import Page
 from .serializers import PageSerializer
@@ -11,6 +12,7 @@ class PageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Page.objects.all()
     serializer_class = PageSerializer
     lookup_field = 'slug'
+    # permission_classes = (AllowAny,)
 
     def retrieve(self, request, *args, **kwargs):
         if 'sleep' in request.GET:
--- resume/apps/resumes/api/views.py
+++ resume/apps/resumes/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from resume.apps.resumes.models import Resume
 from .serializers import ResumeSerializer
@@ -7,3 +8,4 @@ from .serializers import ResumeSerializer
 class ResumeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
+    permission_classes = (AllowAny,)
